fcc05a575c6d1ff23eb1c7b58d67b698dd38cc6b
bogunowicz@arrival.com <bogunowicz@arrival.com>
2022-03-15 14:29:45 +0100

    ready to distribute

--- ac_dc_4_dan/big_ac_dc/ac_dc_run.sh
+++ ac_dc_4_dan/big_ac_dc/ac_dc_run.sh
@@ -5,6 +5,7 @@ python integrations/pytorch/train.py \
     --test-batch-size 256 \
     --loader-num-workers 8 \
     --arch-key mobilenet \
+    --save-epochs 195 295 395 495 595 \
     --pretrained False \
     --dataset imagenette \
     --optim-args '{"momentum": 0.875, "weight_decay": 0.00003051757813}' \
