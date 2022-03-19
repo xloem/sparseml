e4ddfb6fdbbe07ebaa376cb2cb0afede6d131d06
Tuan Nguyen <tuan@neuralmagic.com>
2022-03-19 00:35:33 -0400

    Using teacher tokenizer

--- src/sparseml/transformers/question_answering.py
+++ src/sparseml/transformers/question_answering.py
@@ -419,15 +419,24 @@ def main():
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
     )
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name
-        if model_args.tokenizer_name
-        else model_args.model_name_or_path,
-        cache_dir=model_args.cache_dir,
-        use_fast=True,
-        revision=model_args.model_revision,
-        use_auth_token=True if model_args.use_auth_token else None,
-    )
+    if model_args.distill_teacher is not None:
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.distill_teacher,
+            cache_dir=model_args.cache_dir,
+            use_fast=True,
+            revision=model_args.model_revision,
+            use_auth_token=True if model_args.use_auth_token else None,
+        )
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.tokenizer_name
+            if model_args.tokenizer_name
+            else model_args.model_name_or_path,
+            cache_dir=model_args.cache_dir,
+            use_fast=True,
+            revision=model_args.model_revision,
+            use_auth_token=True if model_args.use_auth_token else None,
+        )
     model, teacher = SparseAutoModel.question_answering_from_pretrained_distil(
         model_name_or_path=model_args.model_name_or_path,
         model_kwargs={
